f01b58ba5a9088d44a2adeaefa7bc933fcefc8cf
Sheldon Clark <sheldonclark@outlook.com>
2016-10-20 13:49:43 -0400

    made cat attributes of name, preferred food, and meal time

--- cat.rb
+++ cat.rb
@@ -1,4 +1,8 @@
 class Cat
 #new class for instances of cats
-
+	def initialize(name, preferred_food, meal_time)
+		@name = name
+		@preferred_food = preferred_food
+		@meal_time = meal_time
+	end
 end
