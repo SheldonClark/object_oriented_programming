517214f79935b33098f11ff8957b785c3c755d22
Sheldon Clark <sheldonclark@outlook.com>
2016-10-20 13:57:31 -0400

    new method eats_at return string of when the cat eats

--- cat.rb
+++ cat.rb
@@ -5,6 +5,11 @@ class Cat
 		@preferred_food = preferred_food
 		@meal_time = meal_time
 	end
+
+	def eats_at
+	#returns when the cat eats at
+		"#{@name} eats at #{@meal_time} PM."
+	end
 end
 
 garfield = Cat.new(garfield, lasagna, 12)
