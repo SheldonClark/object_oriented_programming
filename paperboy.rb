9a70ad86e556273d4e194031dff76087130e80cb
Sheldon Clark <sheldonclark@outlook.com>
2016-10-20 16:45:39 -0400

    calculate earnings and experience for instances of Paperboy

--- paperboy.rb
+++ paperboy.rb
@@ -4,5 +4,37 @@ class Paperboy
 		@name = name
 		@experience = experience
 		@earnings = earnings
+
+	end
+
+	def quota
+			@experience / 2 + 50
+	end
+
+	def deliver(start_address, end_address)
+		@start_address = start_address
+		@end_address = end_address
+		if num_houses == quota
+			@earnings = num_houses * 0.25
+		elsif num_houses > quota
+			@earnings = (num_houses - quota) * 0.50 + 12.5
+		else
+			@earnings = (num_houses * 0.25) - 2
+		end
+		@experience = @experience + num_houses
+		puts "earnings: #{@earnings}"
+		puts @experience
+	end
+
+	def num_houses
+		(@end_address - @start_address).abs
+	end
+
+	def report
+		"I'm #{@name}, I've delivered #{@experience} papers and I've earned $#{@earnings} so far!"
 	end
 end
+mike = Paperboy.new(mike, 0, 0)
+ mike.deliver(120, 200)
+ mike.deliver(200, 120)
+ puts mike.report
